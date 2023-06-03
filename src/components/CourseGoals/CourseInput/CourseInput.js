734a67b3536f299696b859c5bbd0cbd5674fd297
RukeshB <rukeshbasukala@gmail.com>
2023-06-03 17:49:26 +0545

    added conditional styling in input field

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -4,22 +4,28 @@ import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = props => {
+  const [isValid, setIsValid] = useState(true);
   const [enteredValue, setEnteredValue] = useState('');
 
   const goalInputChangeHandler = event => {
+    setIsValid(true)
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length === 0){
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+        <input type="text" style={{borderColor: !isValid ? 'red' : '#ccc'}} onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
